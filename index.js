d00d596467e88f1870ba8debdf9ced753022e04f
Cecil Verghese <cecil.verghese@gmail.com>
2018-02-25 04:25:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,7 +83,13 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if !(cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  }
 }
 
 function getPrice(max) {

6790de233fe928f2083978068f06d1a7a4bff766
Cecil Verghese <cecil.verghese@gmail.com>
2018-02-25 03:51:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ function total() {
   var c = cart.slice(0,cart.length);
   var totPrice = 0;
   var tmpGroceryObj = {};
+  var name = "";
 
   while (c.length > 0) {
     tmpGroceryObj = c.shift();

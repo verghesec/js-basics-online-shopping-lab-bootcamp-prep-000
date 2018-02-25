fc60294df6df301a1d8664777d68bde8775d59ac
Cecil Verghese <cecil.verghese@gmail.com>
2018-02-25 03:01:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,47 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var groceryItemPrice = new Object();
+  groceryItemPrice[item] = getPrice(100);
+  cart.push(groceryItemPrice);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var c = cart;
+  var intro = "In your cart, you have";
+  var separator = "";
+  var final_prefix = ""
+  var output = "";
+  var name = "";
+  var price = 0;
+  var tmpGroceryObj = {};
+
+  console.log(`cart length = ${c.length}`);
+  if ( c.length === 0 ) {
+    console.log("Your shopping cart is empty.");
+  }
+  else if ( c.length > 0 ) {
+    let i = 0;
+    if ( c.length > 2) {
+      separator = ",";
+    }
+    output = intro;
+    while (c.length > 0) {
+      if ( (c.length === 1) && (cart.length > 1) ) {
+        final_prefix = " and";
+      }
+      tmpGroceryObj = c.shift();
+      name = Object.keys(tmpGroceryObj).shift();
+      price = tmpGroceryObj[name]
+      output = output + separator + final_prefix + " " + name + " at " + price;
+    }
+    output = output + ".";
+  }
+
+  console.log(output);
+
 }
 
 function total() {
@@ -27,4 +63,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+}
+
+function getPrice(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
